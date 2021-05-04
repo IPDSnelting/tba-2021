fa0abb7f24c877caec0576aa959366d442acb9f9
Jakob von Raumer <jakob@von-raumer.de>
2021-05-04 15:03:59 +0200

    undo accidental leak of attestation

--- TBA/Solutions/Exercise2.lean
+++ TBA/Solutions/Exercise2.lean
@@ -9,23 +9,21 @@ example : (∀ x, p x) → (∀ x, p x → q x) → (∀ x, q x) :=
 
 -- The reverse direction of the slides example
 example : (∀ x, p x ∧ q x) → (∀ x, p x) ∧ (∀ x, q x) := 
-  fun hpq => ⟨fun x => (hpq x).left, fun x => (hpq x).right⟩ 
+  -- for the `⟨...⟩` and `.left/right` notations, see Solutions/Exercise1.lean
+  fun hpq => ⟨fun x => And.left (hpq x),
+              fun x => (hpq x).right⟩
 
 -- TODO: Prove the corresponding forward/reverse lemma(s) for `∨` (that hold)!
 -- hint: input `∀` as `\all`
-
-example : (∀ x, p x) ∨ (∀ x, q x) → (∀ x, p x ∨ q x) := 
-  fun
-  | Or.inl hp => fun x => Or.inl (hp x)
-  | Or.inr hq => fun x => Or.inr (hq x)
-
--- Die andere Richtung (∀ x, p x ∨ q x) → (∀ x, p x) ∨ (∀ x, q x) funktioniert im Allgemeinen nicht:
--- Mit q = ¬p gilt die Aussage bspw. links zwar immer (unter dem Law of excluded middle), aber die rechte Aussage muss nicht zwingend gelten.
--- Sehr gut!
+example : (∀ x, p x) ∨ (∀ x, q x) → (∀ x, p x ∨ q x) :=
+  -- We can match multiple terms separated by commas in parallel
+  -- Here `fun | ...` is short for `fun h x => match h, x with | ...`
+  fun | Or.inl hp, x => Or.inl (hp x)
+      | Or.inr hq, x => Or.inr (hq x)
 
 -- We can bind multiple variables in a single `∀`
 example : (∀ x y, r x y) → (∀ y x, r x y) := 
-  fun hr hy hx => hr hx hy
+  fun h y x => h x y
 
 /- EXISTENTIAL QUANTIFICATION -/
 
@@ -40,17 +38,17 @@ That is, `Exists.intro` takes/offers a "witness" and a proof that the predicate 
 Instead of `Exists (fun x => p x)`, we can also write `∃ x, p x` (input `∃` as `\ex`).
 -/
 
-example : (∃ x, p x ∧ q x) → (∃ x, p x) ∧ (∃ x, q x) :=
-  fun | Exists.intro hx hpq => ⟨Exists.intro hx hpq.left, Exists.intro hx hpq.right⟩ 
+example : (∃ x, p x ∧ q x) → (∃ x, p x) ∧ (∃ x, q x) := 
+  fun | Exists.intro x hpq => ⟨Exists.intro x hpq.left, Exists.intro x hpq.right⟩
 
-example: ¬(∃ x, p x) → (∀ x, ¬ p x) := 
-  fun hnp x px => hnp (Exists.intro x px)
+example : ¬(∃ x, p x) → (∀ x, ¬ p x) := 
+  fun h x hp => h ⟨x, hp⟩
 
 example : (∀ x, ¬ p x) → ¬(∃ x, p x) := 
-  fun all => fun | Exists.intro x px => (all x) px
+  fun hnotp ⟨x, hpx⟩ => hnotp x hpx
 
 example : (∃ x, ¬ p x) → ¬ (∀ x, p x) := 
-  fun | Exists.intro x px => fun all => px (all x)
+  fun ⟨x, hnotpx⟩ hp => hnotpx (hp x)
 
 section
 open Classical
@@ -59,15 +57,8 @@ open Classical
 -- hint: use the following helper theorem that can be derived from `em`:
 #check byContradiction
 -- hint: you may even need to use it more than once
-
-theorem allex : ¬(∀ x, p x) → (∃ x, ¬ p x) := 
-  fun all =>
-    let h1 : ¬(∃ x, ¬ p x) → (∀ x, p x) := fun hnnp hx => 
-      byContradiction fun npx => hnnp (Exists.intro hx npx)
-    let h2 : ¬(∃ x, ¬ p x) → False := fun hxnp => all (h1 hxnp)
-    byContradiction h2    
--- Um dieses Theorem wiederzuverwenden, habe ich ihm einen Namen gegeben
-
+example : ¬(∀ x, p x) → (∃ x, ¬ p x) := 
+  fun hnotp => byContradiction (fun h => hnotp (fun x => byContradiction (fun hpx => h ⟨x, hpx⟩)))
 end
 
 -- TODO: Decide for yourself what variables you need to model and prove the following
@@ -78,42 +69,28 @@ end
 --   or, more simply, directly as a type "Occupant" since we are not interested in persons outside the pub
 -- hint: you might need classical logic again
 section Drinker
+
 open Classical
-
--- So hat dein Pub genau zwei Gäste. Gemeint hatten wir eigentlich eher `variable (Occupant : Type)`, dann ist es
--- ein beliebiger Typ von Gästen des Pubs.
-inductive Occupant : Type where
-  | drinker : Occupant
-  | nodrinker : Occupant
-
--- Auch hier wäre entsprechend ein beliebiges Prädikat `variable (drinks : Occupant → Prop)` allgemeiner gewesen.
-def drinks : Occupant → Prop := fun w =>
-  match w with
-  | Occupant.drinker => true
-  | Occupant.nodrinker => false
-  
--- Diese Variablen brauchst du nicht, weil das Theorem ja keine Freien Variablen hat, d.h. alle Variablennamen werden dort in Quantoren oder
--- Funktionen eingeführt.
---variable (x y z : Occupant) 
-
-theorem DrinkerParadox : (x : Occupant) → (∃ y, drinks y → (∀ z, drinks z)) :=
-  fun hx => let hp := (∀ y, drinks y) match em hp with
-    | Or.inl h1 => Exists.intro hx (fun hy => h1)
-    | Or.inr h0 => match allex Occupant drinks h0 with
-      | Exists.intro hy ndy => Exists.intro hy (fun dhy => False.elim (ndy dhy))
+-- NOTE: instead of `(∃ p : Occupant, True)`, the equivalent simple `Occupant` could also be used
+example (Occupant : Type) (drinking : Occupant → Prop) : (∃ p : Occupant, True) →
+    ∃ p, drinking p → ∀ p', drinking p' :=
+  fun ⟨p, _⟩ => match em (∃ p', ¬ drinking p') with
+    | Or.inl ⟨p', hp'nodrink⟩ => ⟨p', fun hp'drink => False.elim (hp'nodrink hp'drink)⟩
+    | Or.inr h => ⟨p, fun hpdrink p' => byContradiction (fun hp'nodrink => h ⟨p', hp'nodrink⟩)⟩
 
 end Drinker
 
 /- EQUALITY -/
 
 example : ∀ a b c : α, a = b → b = c → a = c := 
-  fun ha hb hc hab => fun | rfl => hab
+  fun a b c hab hbc => hab ▸ hbc
 
 example : ∀ a : α, ∃ b : α, b = a := 
-  fun ha => Exists.intro ha rfl
+  fun a => ⟨a, rfl⟩
 
 -- "`Eq` is the least reflexive relation"
 example : (∀ a, r a a) → (∀ a b, a = b → r a b) := 
-  fun raa ha hb => fun | rfl => raa ha
+  fun hrrefl a b hab =>
+    hab ▸ hrrefl a
 
 end
