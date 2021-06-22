c6aa456708ab2c16b6a23db0ccb0f01e0d31a519
MXXZ <uxnni@student.kit.edu>
2021-06-22 16:44:20 +0200

    added some theorems in path

--- TBA/Eulerian/Path.lean
+++ TBA/Eulerian/Path.lean
@@ -54,5 +54,53 @@ def hasEqualInOutDegrees (E : List (α × α)) : Prop := ∀ a : α, inDegree E 
 
 def isEulerian (E : List (α × α)) : Prop := ∃ E' : List (α × α), ∃ a : α, E' ≃ E ∧ circuit E' a a 
 
+theorem pathNil {p : List (α × α)} {a b : α} (hp : path p a b) : p = [] → a = b := by 
+  intro heq 
+  rw [heq] at hp
+  match hp with 
+  | path.refl => rfl 
+
+theorem pathAppend {p q : List (α × α)} {a b c : α} 
+  (hp : path p a b) (hq : path q b c) : path (p++q) a c := by 
+  induction p generalizing a b with 
+  | nil => 
+    have heq : a = b := pathNil hp (by rfl) 
+    rw [nil_append] 
+    rw [← heq] at hq 
+    exact hq 
+  | cons e p' ih => 
+    cases hp with 
+    | trans e p' hp' => 
+      have h := ih hp' hq
+      rw [cons_append]
+      exact path.trans e (p'++q) h
+
+-- Inverse to pathAppend.
+theorem pathBreak {p q : List (α × α)} {a b c : α}  
+  (hpq : path (p++q) a c) : ∃ b : α, path p a b ∧ path q b c := by 
+  induction p generalizing a c with 
+  | nil => 
+    rw [nil_append] at hpq 
+    exact ⟨a, path.refl, hpq⟩ 
+  | cons e p' ih => 
+    rw [cons_append] at hpq 
+    cases hpq with 
+    | trans _ _ hpq' => 
+      have ⟨b, hp', hq⟩ := ih hpq' 
+      exact ⟨b, path.trans e p' hp', hq⟩ 
+
+theorem pathLastEdge {p : List (α × α)} {e : (α × α)} {a b : α} 
+  (hp : path (e::p) a b) : ∃ x : α × α, x ∈ e::p ∧ x.2 = b := by 
+  induction p generalizing e a b with 
+  | nil => 
+    cases hp with 
+    | trans _ _ hp' => 
+      have heq := pathNil hp' $ rfl 
+      exact ⟨e, Mem.head e [], heq⟩ 
+  | cons e' p' ih => 
+    cases hp with 
+    | trans _ _ hp' => 
+      have ⟨x, hin, heq⟩ := ih hp'
+      exact ⟨x, Mem.tail x e (e'::p') hin, heq⟩ 
 
 end Eulerian
